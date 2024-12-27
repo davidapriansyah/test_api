926940ea2499637dc4e14a2c32fbb2d98eb01e4f
david apriansyah <davidapriansyah@davids-MacBook-Air.local>
2024-12-27 18:35:16 +0700

    set up router

---
+++ controllers/AuthController.js
@@ -0,0 +1,19 @@
+
+
+class AuthController {
+    static async register(req, res, next) {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async login(req, res, next) {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
