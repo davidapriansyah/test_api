fc0c3f9a1a65e48ff6a6bbcfd7b933bcd4a88744
david apriansyah <davidapriansyah@davids-MacBook-Air.local>
2024-12-29 13:46:27 +0700

    fix: finish

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
 if(process.env.NODE_ENV !== "production"){
     require("dotenv").config()
 }
-
+const PORT = process.env.PORT || 3000
 const express = require("express")
 const router = require("./routers/index.js")
 const app = express()
@@ -10,4 +9,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(router)
 
-module.exports=app
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+    
+})
